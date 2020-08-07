7daf9cdd111976c3492ebb7c230e7ea74bef6cf4
YnYnHnFn <buchi4126@yahoo.co.jp>
2020-08-07 20:32:37 +0900

    手順 5: Django でユーザーを認証する

--- DjangoWeb/DjangoWeb/urls.py
+++ DjangoWeb/DjangoWeb/urls.py
@@ -21,6 +21,12 @@ urlpatterns = [
     path('login/',
          LoginView.as_view
          (
+            # template_name は、ログイン ページ用のテンプレート (この場合は templates/app/login.html) を特定します。
+            # extra_context プロパティは、テンプレートに指定された既定のコンテキスト データに追加されます。 
+            # authentication_form は、テンプレートに form オブジェクトとして示される、ログインに使用するフォーム クラスを定義します。
+            # 既定値は AuthenticationForm (django.contrib.auth.views より) です。
+            # Visual Studio プロジェクト テンプレートでは代わりに、アプリの forms.py ファイルに定義された次のフォームを使用します。
+
              template_name='app/login.html',
              authentication_form=forms.BootstrapAuthenticationForm,
              extra_context=
@@ -32,7 +38,17 @@ urlpatterns = [
          name='login'),
 
     #組み込みの Django ビューを使用します。
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    #path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+
+    path('logout/', 
+         LogoutView.as_view
+        (
+            template_name='app/loggedoff.html',
+            # 'next_page': '/',
+        ),
+        name='logout'),
+
 
     path('admin/', admin.site.urls),
+
 ]
--- DjangoWeb/app/forms.py
+++ DjangoWeb/app/forms.py
@@ -1,5 +1,13 @@
 """
 Definition of forms.
+
+このフォーム クラスは AuthenticationForm (django.contrib.auth.views)
+から派生し、明示的にユーザー名とパスワードのフィールドをオーバーライド
+して、プレースホルダ― テキストを追加します。 
+Visual Studio テンプレートには、パスワード強度の検証を追加するなど、
+必要に応じてフォームをカスタマイズすることを予想して、この明示的な
+コードが含まれています。
+
 """
 
 from django import forms
@@ -7,11 +15,14 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
 
 class BootstrapAuthenticationForm(AuthenticationForm):
+
     """Authentication form which uses boostrap CSS."""
+
     username = forms.CharField(max_length=254,
                                widget=forms.TextInput({
                                    'class': 'form-control',
                                    'placeholder': 'User name'}))
+
     password = forms.CharField(label=_("Password"),
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
